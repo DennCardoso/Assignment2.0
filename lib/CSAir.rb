8eea1c1d86d2e666a5e5cfc960338bbd6e963924
denniscardoso <dennis.cardoso.122@gmail.com>
2015-02-25 03:55:09 -0600

    new dictories

--- lib/CSAir.rb
+++ lib/CSAir.rb
@@ -1,5 +1,15 @@
-require "CSAir/version"
+require "rubygems"
+require "json"
 
 module CSAir
-  push "Hello World"
+  puts __FILE__
+
+  #!config_path = File.expand_path(File.join(File.dirname(__FILE__), "test.json"))
+  config_path = File.expand_path("../../Resources/test.json", __FILE__)
+
+  puts config_path
+  file = File.read(config_path)
+  data_hash = JSON.parse(file)
+
+  puts(data_hash)
 end
